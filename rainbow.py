89190b98ee3e0eae7c4a1d21d1245e2f8df45d36
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-07-20 21:40:51 +0530

    LOOPS

--- rainbow.py
+++ rainbow.py
@@ -10,14 +10,14 @@ t.speed("fastest")
 t.hideturtle()
 
 while True:
-    for x in range(200):
+    for x in range(350):
         t.pencolor(colors[x % len(colors)])
         t.width(x/100 +1)
-        t.forward(xgit )
+        t.forward(x)
         t.left(59)
 
     t.right(239)
-    for x in range(200, 0 , -1):
+    for x in range(350, 0 , -1):
         t.pencolor("black")
         t.width(x/100 + 7)
         t.forward(x)
