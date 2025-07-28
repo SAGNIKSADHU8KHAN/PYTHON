235f6b6b7c68509c8c3d0cff8c1e2a64be8ee2d4
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-07-28 10:16:48 +0530

    DATA STRUCTURE IN PYTHON

--- operationondictionary.py
+++ operationondictionary.py
@@ -6,4 +6,24 @@ my_dict = { }
 
 my_dict = {1: "apple", 2: "banana"}
 
-my_dict = {"name" : "John", 1 , }
+my_dict = {"name" : "John", 1: [1,2,4] }
+
+my_dict = {"name": "Jack" , "age" : 18}
+
+print(my_dict ["name"])
+print(my_dict.get ("age"))
+
+my_dict["age"] = 27
+print(my_dict)
+
+my_dict["address"] = "Howrah"
+print(my_dict)
+
+my_dict.pop("age")
+print(my_dict)
+
+print("Address :", my_dict.get("address"))
+
+my_dict.clear()
+print(my_dict)
+
