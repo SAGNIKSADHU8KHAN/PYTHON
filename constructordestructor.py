b36c22bf7d703c7b635028831c90523dcbe64d3f
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-08-03 19:42:07 +0530

    constructordestructor

---
+++ constructordestructor.py
@@ -0,0 +1,10 @@
+class Employee:
+
+    def __init__(self):
+        print("Employee created")
+
+    def __init__(self):
+        print("Destructor called, employee deleted")
+
+Raju = Employee()
+del Raju
