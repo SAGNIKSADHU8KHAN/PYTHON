8a7e6505db08011104eea00e07ee3e12c03c212f
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-08-09 20:01:24 +0530

    inheritance

---
+++ employeedetails.py
@@ -0,0 +1,23 @@
+class Person(object):
+
+    def __init__(self, name, idname):
+        self.name = name
+        self.idname = idname
+
+    def display(self):
+        print(self.name)
+        print(self.idname)
+
+class Employee(Person):
+
+    def __init__(self, name, idname, salary, post):
+
+        self.salary = salary
+        self.post = post
+        Person.__init__(self, name, idname)
+
+aloo = Employee("Aloo", 234534, 25000, "assistant")
+
+aloo.display()
+print(aloo.post)
+print(aloo.salary)
--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import random
 
 def number_guessing_game():
-    number_to_guess = random.randict(1,100)
-    guess_history = []
+    number_to_guess = random.randint(1, 100)
+    guess_history = []  
     attempts = 0
 
-print(" Welcome to number guessing game !")
-print(" Guess the number between 1 to 100 ")
+    print("Welcome to the Number Guessing Game!")
+    print("Guess the number between 1 and 100.")
 
-while True:
-     try:
-        guess = int(input('Enter an number'))
-        attempts += 1
-        guess_history.append(guess)
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+            guess_history.append(guess)
 
-        if guess < number_to_guess:
-            print("Too low ! Try again.")
+            if guess < number_to_guess:
+                print("Too low! Try again.")
+            elif guess > number_to_guess:
+                print("Too high! Try again.")
+            else:
+                print(f"🎉 Congratulations! You guessed the number in {attempts} attempts.")
+                break
 
+        except ValueError:
+            print("Invalid input! Please enter an integer.")
 
+    print("\nYour Guess History:", guess_history)
+
+# Run the game
+number_guessing_game()
