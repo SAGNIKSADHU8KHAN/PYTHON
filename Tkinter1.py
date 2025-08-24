2af069aa509e74e3537fbdd88ab8455b7c4ff8c1
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-08-24 20:00:00 +0530

    Tkinter

---
+++ Tkinter1.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+window = Tk()
+window.title("Tkinter sample windoe")
+window.geometry("300x300")
+
+greeting = Label(text = "Hello user", fg = "black", bg = "white")
+
+button = Button(text= "Click me", bg = "black", fg = "white")
+
+entry = Entry(fg = "yellow", bg = "blue", width =50)
+
+greeting.pack()
+button.pack()
+entry.pack()
+
+frame = Frame(master=window, relief=RAISED, borderwidth=5)
+frame.pack()
+
+lable = Label(master=frame, text="Sample frame")
+lable.pack()
+
+textBox = Text(fg = "green", bg ="yellow")
+textBox.pack()
+
+window.mainloop()
