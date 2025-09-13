5c0ee461929a70dc38ff137802e6e599727d431d
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-09-13 20:06:55 +0530

    BIG O NOTATION

---
+++ o_of_1.py
@@ -0,0 +1,10 @@
+def printNumber(n):
+    
+    iteration = 0
+    print("Print the nymber inserted", n)
+    iteration += 1
+    print("The number of iteration", iteration)
+
+
+printNumber(100)
+printNumber(10000000000)
---
+++ o_of_n.py
@@ -0,0 +1,10 @@
+def OnTime(n):
+    iteration = 0
+    for i in range(1, n+1):
+        iteration+= 1
+
+    print("When n is ", n ,"then iteration is ", iteration)
+
+OnTime(20)
+OnTime(100)
+      
