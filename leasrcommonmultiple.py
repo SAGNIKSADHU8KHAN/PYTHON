a5db6f4926505d623239ec00423586e1b5437757
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-09-29 09:46:56 +0530

    Mathematical Magic

---
+++ leasrcommonmultiple.py
@@ -0,0 +1,12 @@
+import math
+
+def find_lcm(a, b):
+
+    return(a*b)// math.gcd(a,b)
+
+num1 = int(input("Enter the first number"))
+num2 = int(input("Enter the second number "))
+
+lcm = find_lcm(num1, num2)
+
+print(f"The LCM of {num1} and {num2} is ", lcm)
