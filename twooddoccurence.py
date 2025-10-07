b5227472b06945fa40385c5e516ac079e0c91406
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-10-07 10:31:42 +0530

    BIT MANUPULATION

---
+++ twooddoccurence.py
@@ -0,0 +1,30 @@
+def PrintTwoOdd(arr, size):
+
+    xorof2 = arr[0]
+
+    x = 0
+    y = 0 
+
+    Setbit = 0 
+
+    for i in range(1, size):
+        xorof2 = xorof2 ^ arr[i]
+
+    setbit = xorof2 & ~(xorof2 - 1)
+
+    for i in range(size):
+        if(arr[i] & setbit):
+            x = x ^ arr[i]
+        else:
+            y = y ^ arr[i]
+
+    print("The two odd elements are", x, "&", y)
+
+arr = []
+
+arr_size = int(input("Enter the size of the array :"))
+for i in range(0, arr_size):
+    z = int(input("Enter element :"))
+    arr.append(z)
+
+PrintTwoOdd(arr, arr_size)
