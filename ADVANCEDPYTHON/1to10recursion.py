da59cf22f2304de9b3789331fbcf02751e8bba13
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-10-25 19:56:38 +0530

    RECURSION

---
+++ ADVANCEDPYTHON/1to10recursion.py
@@ -0,0 +1,10 @@
+def print1to10(n):
+
+    if n > 10:
+        return
+
+    print(n)
+    print1to10(n+1)
+
+print1to10(1)
+
---
+++ ADVANCEDPYTHON/factorialrecursion.py
@@ -0,0 +1,11 @@
+def factorial(n):
+
+    if(n ==0 or n == 1):
+        return 1
+    
+    return n*factorial(n-1)
+
+number = int(input("Enter any number "))
+
+print(f"The factorial of {number} is", factorial(number))
+
