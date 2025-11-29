435420c186495e68fe94b4485d0806a4b352f7a6
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-11-29 18:42:17 +0530

    ARRAY PROBLEMS 1

---
+++ ADVANCEDPYTHON/missingnumber.py
@@ -0,0 +1,14 @@
+def find_missing(arr):
+
+    n = len(arr) + 1
+
+    expected_sum = n * (n+1)//2
+
+    actual_sum = sum(arr)
+
+    return expected_sum - actual_sum
+
+
+arr = [1, 4, 3, 2, 6]
+
+print("Missing number:", find_missing(arr))
