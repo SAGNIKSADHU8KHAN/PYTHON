baad302dbdd63d13ece567b218cbf963c111e79d
SAGNIKSADHUKHAN <lakshmisadhukhan@95.com>
2025-11-30 20:10:33 +0530

    KADANE ALGORITHM

---
+++ ADVANCEDPYTHON/kadanealgo.py
@@ -0,0 +1,19 @@
+def maxsubarray(a, a_size):
+
+    max = -999999999999
+    cmax = 0
+
+    for i in range(0, a_size):
+
+        cmax = cmax = a[i]
+
+        if(max < cmax):
+            max = cmax
+
+        if cmax< 0:
+            cmax = 0
+
+    return max
+
+a = [1,2,3,-4,5,-22,-24,2,9]
+print(maxsubarray(a,len(a)))
